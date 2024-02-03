738ce99b5b594e97f967301371f10fc3b257c8f5
Hoerudin <hoerudinpermana30@gmail.com>
2024-02-03 22:58:04 +0700

    views

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'hoerudinpermana30@gmail.com';
+    public string $fromName   = 'Hoerudin';
     public string $recipients = '';
 
     /**
